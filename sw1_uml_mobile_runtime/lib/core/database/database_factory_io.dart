1e61552a73a1b4b570efce1f46b3df39501f87de
Stefy127 <noeste1234@gmail.com>
2026-09-16 22:25:46 -0400

    feat: complete Android offline runtime and sync

--- sw1_uml_mobile_runtime/lib/core/database/database_factory_io.dart
+++ sw1_uml_mobile_runtime/lib/core/database/database_factory_io.dart
@@ -1,5 +1,10 @@
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
 
-Future<Database> openRuntimeDatabase(String name) =>
-    databaseFactoryIo.openDatabase(name);
+Future<Database> openRuntimeDatabase(String name) async {
+  final directory = await getApplicationSupportDirectory();
+  final databasePath = p.join(directory.path, name);
+  return databaseFactoryIo.openDatabase(databasePath);
+}
